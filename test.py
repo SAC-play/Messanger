eb0de9134919e69b9ba3799c129c84e3f85ace21
prksnw <psw9728@naver.com>
2021-03-27 10:57:18 +0900

    change codes as new policy for Slack

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-from slacker import Slacker
-
-slack = Slacker('xoxb-1829797681845-1907933122324-wZHxIIeCCHf3iTyOJ0wMGZMP')
-
-# Send a message to #general channel
-slack.chat.post_message('#bott', 'Hello fellow slackers!')
+import requests
+ 
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = "xoxb-1829797681845-1910880424580-nKuFbTsHvjGzp2THw4S1gu1Z"
+ 
+post_message(myToken,"#bott","Hello Slack World")
